e6d1c76021ab45ab951e3eb0dc50067a5a34b847
Errol Vidad <errolvidad@gmail.com>
2023-10-12 05:32:29 +0000

    finished lab6

--- code-102-notes/apps.js
+++ code-102-notes/apps.js
@@ -1,71 +1,10 @@
-// Variables 
 
-// Variable is a container for information (data)
+const refreshButton = document.getElementById("refreshButton");
 
-// 4 ways to declare a variable
 
-// 1:let can be changed
-// 2:const cannot be changed
-// 3: var <-- don't use
-// 4: (none) <-- don't use
+function refreshContent() {
 
-const myName = 'Errol';
-
-let myAge = 34;
-// birthday day happens
-myAge = 35;
-
-// Data type - has quotation marks
-// strings: text - has quotation marks
-// boolean: True or false
-// number: no quotation marks
-
-// assignment operator
-let x = 5
-let y = 10
-
-// == loosely equivalent, data types do not need to match
-// === strictly equivalent, data types must match
-
-// x == y (FALSE)
-
-//conditional logic
-
-//if (this is true) {execute this code}
-
-let x = 5
-let y = 10
-
-if (x == y) {
-    console.log('Yes it is');
- }
-else {
-    consolve.log('No it isnt');
+    const contentElement = document.querySelector("p");
+    contentElement.textContent = "Content refreshed!";
 }
 
-
-<!-- <script>            -->
-const usersAnswer = prompt('What is your favorite Lorem ipsum?');
-
-console.log('User typed: ' + usersAnswer);
-
-document.write('Hi ' + usersName);
-
-console.log(usersName);
-
-document.write('Hi ' + usersName);
-
-let x = 5
-let y = 5
-
-if (x == y) {
-console.log('Yes it is');
-    }
-else {
-console.log('No it isnt');
-}
-
-
-document.write('Thank you very much'); 
-
-<!-- </script> -->

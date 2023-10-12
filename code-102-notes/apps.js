ebefef6c31e13cf5df5f9d3bb5fe1fffaf71a3d7
Errol Vidad <errolvidad@gmail.com>
2023-10-12 20:56:02 +0000

    added loop

--- code-102-notes/apps.js
+++ code-102-notes/apps.js
@@ -24,12 +24,16 @@
 alert('Welcome!');
 
 function askFlavor() {
-    let usersFlavor = prompt('What is your favorite poke flavor?');
+    let usersFlavor = prompt('What kind of poke is my favorite? (null for answer 1; spicy ahi for answer 2; anything else for answer 3; nah to repeat question)');
     console.log(usersFlavor);
     if (usersFlavor === null || usersFlavor.trim() === "") {
         document.write('I know you did not write anything, but here are some of my favorites anyway.');
+    } else if (usersFlavor == 'spicy ahi') {
+        document.write('that is my favorite and here are some more!');
+    } else if (usersFlavor == 'nah') {
+        usersFlavor = prompt('What kind of poke is my favorite? (null for answer 1; spicy ahi for answer 2; anything else for answer 3)');
     } else {
-        document.write(usersFlavor + ' is my favorite too!');
+        document.write(usersFlavor + ', are you even sure that is a poke?');
     }
     // document.write(usersFlavor + ' is my favorite too! Here are some of my recommendations!');
   }

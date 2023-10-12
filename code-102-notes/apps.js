97e6473cd093edbb8878ae6d76d4d3e18b456361
Errol Vidad <errolvidad@gmail.com>
2023-10-12 16:45:24 +0000

    don't know how this works but it works

--- code-102-notes/apps.js
+++ code-102-notes/apps.js
@@ -1,10 +1,1 @@
 
-const refreshButton = document.getElementById("refreshButton");
-
-
-function refreshContent() {
-
-    const contentElement = document.querySelector("p");
-    contentElement.textContent = "Content refreshed!";
-}
-
